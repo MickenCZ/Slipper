d9cadc940c2cd4c1394a4aa0e7ac940ff6fdd868
MickenCZ <MickenCZseznam.cz>
2023-06-10 20:21:05 +0200

    added server-side validation

--- pages/api/createPost.js
+++ pages/api/createPost.js
@@ -1,20 +1,38 @@
 import mysql from "mysql2"
+import initMiddleware from '../../lib/init-middleware'
+import validateMiddleware from '../../lib/validate-middleware'
+import { check, validationResult } from 'express-validator'
 
-export default function handler(req, res) {
-  //const errors = validationResult(req)
-  //if (!errors.isEmpty()) {res.sendStatus(400)}
-  //else {
+const validateBody = initMiddleware(
+  validateMiddleware([
+      check("messageContent").notEmpty().isString().isLength({min:1, max:69}).trim().escape(),
+      check("userName").notEmpty().isString().isLength({min:1, max:100}).trim().escape(),
+      check("pfpURL").notEmpty().isString().isLength({min:3, max:500}).trim().blacklist("<>\"'`")
+  ], validationResult)
+)
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    await validateBody(req, res)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
     const { pfpURL, userName, messageContent } = req.body
-  const connection = mysql.createConnection(process.env.DATABASE_URL)
-  connection.query("INSERT INTO `posts` (`ID`, `pfpURL`, `userName`, `timePosted`, `messageContent`, `likes`, `comments`) VALUES (NULL, '"+ pfpURL +"', '"+ userName +"', '"+ Date.now() +"', '"+ messageContent +"', '0', '0')", (error, results, fields) => {
+    const connection = mysql.createConnection(process.env.DATABASE_URL)
+    connection.query("INSERT INTO `posts` (`ID`, `pfpURL`, `userName`, `timePosted`, `messageContent`, `likes`, `comments`) VALUES (NULL, '"+ pfpURL +"', '"+   userName +"', '"+ Date.now() +"', '"+ messageContent +"', '0', '0')", (error, results, fields) => {
     if (error) throw error;
-    //console.log(results);
   })
 
   connection.end()
   res.status(201).send("Success")
+
   }
-//}
+  else {
+    res.status(404).json({ message: "Request HTTP Method Incorrect." })
+  }
+}
+    
 
 export const config = {
   api: {
